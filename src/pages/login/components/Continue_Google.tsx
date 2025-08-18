994e569d2a71fd75d4e457b05798cffd9731d1e9
kyuris01 <chrisahn1007@gmail.com>
2025-08-18 13:54:41 +0000

    fix : 로그인 로직에서 authUrl 수정

--- src/pages/login/components/Continue_Google.tsx
+++ src/pages/login/components/Continue_Google.tsx
@@ -5,13 +5,14 @@ const Continue_Google = () => {
     const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
     const addr = import.meta.env.VITE_FRONT_URL;
     const redirectUri = encodeURIComponent(`${addr}/login-complete`); // Google 콘솔에 등록한 리디렉션 URI
-    const scope = encodeURIComponent("openid email profile");
+    const scope = encodeURIComponent(
+      "openid email profile https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.readonly"
+    );
     const responseType = "code";
     const accessType = "offline";
     const prompt = "consent";
 
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&access_type=${accessType}&prompt=${prompt}`;
-
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&access_type=${accessType}&include_granted_scopes=true&prompt=${prompt}`;
     window.location.href = authUrl;
   };
 
