067aa1118ae3223e7ce11c9fdec85745a2b529f3
kyuris01 <chrisahn1007@gmail.com>
2025-08-20 18:23:41 +0000

    fix :

--- src/App.tsx
+++ src/App.tsx
@@ -26,55 +26,24 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route
-          index
-          path="/alternative/:id"
-          element={<MeetingAlternativePage />}
-        ></Route>
-        <Route
-          index
-          path="meeting/detail"
-          element={<MeetingDetailPage />}
-        ></Route>
+        <Route index path="/alternative/:id" element={<MeetingAlternativePage />}></Route>
+        <Route index path="/detail" element={<MeetingDetailPage />}></Route>
         <Route path="/login" element={<LoginContainer />}></Route>
-        <Route
-          path="/login-complete"
-          element={<LoginCompleteContainer />}
-        ></Route>
+        <Route path="/login-complete" element={<LoginCompleteContainer />}></Route>
         <Route path="/" element={<BackgroundPage />}>
           <Route index path="schedule" element={<SchedulePage />} />
           <Route path="meeting" element={<MeetingPage />} />
           <Route index path="/memoir" element={<Memoir_meeting_All />}></Route>
-          <Route index path="/mypage" element={<MyPage/>}></Route>
+          <Route index path="/mypage" element={<MyPage />}></Route>
         </Route>
 
-        <Route
-          index
-          path="/memoir-write"
-          element={<Memoir_write_ctn />}
-        ></Route>
-        <Route
-          index
-          path="/memoir-complete"
-          element={<Memoir_complete_ctn />}
-        ></Route>
-        <Route
-          index
-          path="/timetable"
-          element={<Participate_timetable_ctn />}
-        ></Route>
+        <Route index path="/memoir-write" element={<Memoir_write_ctn />}></Route>
+        <Route index path="/memoir-complete" element={<Memoir_complete_ctn />}></Route>
+        <Route index path="/timetable" element={<Participate_timetable_ctn />}></Route>
         <Route index path="/participate" element={<Participate_link />}></Route>
         <Route index path="/error" element={<Participate_error_ctn />}></Route>
-        <Route
-          index
-          path="/complete"
-          element={<Participate_completed_ctn />}
-        ></Route>
-        <Route
-          index
-          path="/meeting-creation"
-          element={<MeetingCreationPage />}
-        ></Route>
+        <Route index path="/complete" element={<Participate_completed_ctn />}></Route>
+        <Route index path="/meeting-creation" element={<MeetingCreationPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
