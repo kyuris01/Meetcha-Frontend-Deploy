aa784d1bf228065c3905a6ad96ef7aac28a53395
kyuris01 <chrisahn1007@gmail.com>
2025-08-20 05:21:15 +0000

    chore: 배경색 변경

--- src/pages/memoir/Memoir_meeting/Meeting_list_content.tsx
+++ src/pages/memoir/Memoir_meeting/Meeting_list_content.tsx
@@ -31,10 +31,11 @@ const Meeting_list_content = ({ meetingLists }: Props) => {
   // 2. 클릭 시 선택된 미팅 ID 저장 + fire 실행
   const handleClick = (meeting: any) => {
     setSelectedMeetingId(meeting.meetingId);
-
-    fire();
-   
   };
+
+  useEffect(() => {
+    if (selectedMeetingId) fire(); // ← 최신 URL로 1회만 호출
+  }, [selectedMeetingId, fire]);
 
   // 3. 회고 데이터 도착하면 이동
   useEffect(() => {
@@ -69,7 +70,9 @@ const Meeting_list_content = ({ meetingLists }: Props) => {
                   >
                     {meeting.projectName ?? "프로젝트 없음"}
                   </div>
-                  <p className="meeting_date">{meeting.confirmedTime.replace("T"," ")}</p>
+                  <p className="meeting_date">
+                    {meeting.confirmedTime.replace("T", " ")}
+                  </p>
                 </div>
                 <div className="meeting_main">
                   <p className="meeting_title">{meeting.title}</p>
