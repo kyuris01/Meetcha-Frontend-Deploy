1b884f1ca206d8d9a73edadfc5dc7a76172dafc8
kyuris01 <chrisahn1007@gmail.com>
2025-08-20 06:09:16 +0000

    chore:meetingListcode 되돌려옴

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import myPageImg from "@/assets/myPage.png";
 import "./MyPage.scss";
+import { apiCall } from "@/apis/apiCall";
+import { useState } from "react";
 const MyPage = () => {
+  const [userData,setUserData]=useState<any>();
+  const getUserData = async () => {
+      
+      try {
+        const res = await apiCall(`/user/mypage`, "GET", null, true);
+  
+        if (!res) return;
+        if (res.code === 404) {
+          alert("존재하지 않는 미팅입니다");
+        } else if (res.code === 401) {
+          alert("인증이 필요합니다");
+        } else if (res.code == 200||res.code==201) {
+          console.log(res);
+          setUserData(res);
+        }
+      } catch (e) {
+        alert("서버 오류");
+      }
+    };
+    useEffect(() => {
+        getUserData();
+        console.log(userData);
+      }, []);
   return (
     <div className="myPage_ctn">
       <div className="myPage_nickname">
