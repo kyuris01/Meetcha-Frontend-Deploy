cf857b153d40f6a8aa668b177055b2213d95aefe
kyuris01 <chrisahn1007@gmail.com>
2025-08-20 02:47:10 +0000

    chore: MyPage페이지 생성

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import myPageImg from "@/assets/myPage.png";
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div className="myPage_ctn">
+      <div className="myPage_nickname">
+        <div className="img_ctn">
+            <img className="myPageImg" src={myPageImg} alt="myPageImg"></img>
+        </div>
+        <div className="nickname_ctn">
+            <p className="nickname_text">닉네임</p>
+            <p className="nickname">Kuit Meetcha</p>
+        </div>
+      </div>
+      <div className="myPage_information">무엇을 입력할까요?</div>
+    </div>
+  );
 };
 
 export default MyPage;
