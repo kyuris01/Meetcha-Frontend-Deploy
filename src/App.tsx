234cbd15e30037866c7e958ff730caa8693f1d02
kyuris01 <chrisahn1007@gmail.com>
2026-01-21 08:56:39 +0000

    refactor(App): lazy loading 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import LoginContainer from "./pages/login/components/LoginContainer";
 import LoginCompleteContainer from "./pages/login_complete/components/LoginCompleteContainer";
 import BackgroundPage from "./pages/background/BackgroundPage";
 import MemoirMeetingAll from "./pages/memoir/MemoirMeeting/MemoirMeetingAll";
 import MemoirWriteCtn from "./pages/memoir/MemoirWrite/MemoirWiteCtn";
 import MemoirCompleteCtn from "./pages/memoir/MemoirComplete/MemoirCompleteCtn";
-import MeetingCreationPage from "./pages/meeting/create/MeetingCreationPage";
 import MeetingDetailPage from "./pages/meeting/detail/MeetingDetailPage";
-import MeetingAlternativePage from "./pages/meeting/alternative/MeetingAlternativePage";
 import MeetingSharePage from "./pages/meeting/share/MeetingSharePage";
-import SchedulePage from "./pages/schedule/SchedulePage";
 import MeetingPage from "./pages/meeting/MeetingPage";
 
 import ParticipateTimetableCtn from "./pages/participate/Participate_timetable/ParticipateTimetableCtn";
@@ -28,41 +26,50 @@ import MeetingLinkPage from "./pages/meeting/link/MeetingLinkPage";
 import { AlreadyAuthRedirect } from "./components/AlreadyAuthRedirect";
 
 import ProtectedRoute from "./ProtectedRoute";
+import Loading from "./components/Loading/Loading";
+
+const MeetingAlternativePage = lazy(
+  () => import("./pages/meeting/alternative/MeetingAlternativePage")
+);
+const MeetingCreationPage = lazy(() => import("./pages/meeting/create/MeetingCreationPage"));
+const SchedulePage = lazy(() => import("./pages/schedule/SchedulePage"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Navigate to="/landingPage" replace />} />
-        <Route element={<AlreadyAuthRedirect />}>
-          <Route path="/landingPage" element={<LandingBackground />} />
-          <Route path="/login" element={<LoginContainer />} />
-        </Route>
-        <Route path="/login-complete" element={<LoginCompleteContainer />}></Route>
-
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/alternative/:id" element={<MeetingAlternativePage />}></Route>
-          <Route path="/meeting/detail" element={<MeetingDetailPage />}></Route>
-          <Route element={<BackgroundPage />}>
-            <Route path="/schedule" element={<SchedulePage />} />
-            <Route path="/meeting" element={<MeetingPage />} />
-            <Route path="/memoir" element={<MemoirMeetingAll />}></Route>
-            <Route path="/mypage" element={<MyPage />}></Route>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Navigate to="/landingPage" replace />} />
+          <Route element={<AlreadyAuthRedirect />}>
+            <Route path="/landingPage" element={<LandingBackground />} />
+            <Route path="/login" element={<LoginContainer />} />
           </Route>
-          <Route path="/memoir-write" element={<MemoirWriteCtn />}></Route>
-          <Route path="/memoir-complete" element={<MemoirCompleteCtn />}></Route>
-          <Route path="/timetable" element={<ParticipateTimetableCtn />}></Route>
-          <Route path="/participate" element={<ParticipateLink />}></Route>
-          <Route path="/error" element={<ParticipateErrorCtn />}></Route>
-          <Route path="/complete" element={<ParticipateCompletedCtn />}></Route>
-          <Route path="/meeting-creation" element={<MeetingCreationPage />}></Route>
-          <Route path="/privacy" element={<PrivacyPage />}></Route>
-          <Route path="/meeting/share/:meetingId" element={<MeetingSharePage />} />
-          <Route path="/meeting/link" element={<MeetingLinkPage />} />
-        </Route>
-      </Routes>
+          <Route path="/login-complete" element={<LoginCompleteContainer />}></Route>
+
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/alternative/:id" element={<MeetingAlternativePage />}></Route>
+            <Route path="/meeting/detail" element={<MeetingDetailPage />}></Route>
+            <Route element={<BackgroundPage />}>
+              <Route path="/schedule" element={<SchedulePage />} />
+              <Route path="/meeting" element={<MeetingPage />} />
+              <Route path="/memoir" element={<MemoirMeetingAll />}></Route>
+              <Route path="/mypage" element={<MyPage />}></Route>
+            </Route>
+            <Route path="/memoir-write" element={<MemoirWriteCtn />}></Route>
+            <Route path="/memoir-complete" element={<MemoirCompleteCtn />}></Route>
+            <Route path="/timetable" element={<ParticipateTimetableCtn />}></Route>
+            <Route path="/participate" element={<ParticipateLink />}></Route>
+            <Route path="/error" element={<ParticipateErrorCtn />}></Route>
+            <Route path="/complete" element={<ParticipateCompletedCtn />}></Route>
+            <Route path="/meeting-creation" element={<MeetingCreationPage />}></Route>
+            <Route path="/privacy" element={<PrivacyPage />}></Route>
+            <Route path="/meeting/share/:meetingId" element={<MeetingSharePage />} />
+            <Route path="/meeting/link" element={<MeetingLinkPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
